37a1a540bad260576ef8cc95c5a269ef77e52257
kkopp <kristinakopp@Kristinas-MacBook-Pro.local>
2019-01-12 11:46:54 -0700

    making all the directory stuff

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Burger Royale with Cheese');
+
